15bba9918e88b154629a19688ab51853fdfcbd1a
Juan Veloza <juanveloza2305@gmail.com>
2022-10-31 19:45:08 -0500

    Commit de Paula Code

---
+++ Pau_with_me/Pau_with_me.py
@@ -0,0 +1,9 @@
+def paula():
+    name = input("¿Cuál es tu nombre?: ")
+    if name == 'Paula':
+        print('Hola Paula')
+    else:
+        print('No te conozco')
+
+if __name__ == '__main__':
+    paula()
